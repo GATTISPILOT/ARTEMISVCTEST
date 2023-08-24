5e5fb60dab43753cae721d27bd4ec215844c5601
gattispilot@yahoo.com <gattispilot@yahoo.com>
2023-08-24 05:33:11 -0500

    Add project files.

---
+++ vcmesh.h
@@ -0,0 +1,21 @@
+// ========================================================
+// Mesh resource file for D:/Orbiter2016\Meshes\SPS_VC.msh
+// Generated with meshc on Mon Oct 24 20:12:18 2022
+
+// ========================================================
+
+// Number of mesh groups:
+#define NGRP 4
+
+// Number of materials:
+#define NMAT 2
+
+// Number of textures:
+#define NTEX 2
+
+// Named mesh groups:
+#define GRP_mesh01 0
+#define GRP_LeftMFDScreen 1
+#define GRP_mesh02 2
+#define GRP_mesh16 3
+#pragma once
